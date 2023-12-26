f4dbb3e615b395cb7281e80dd89ead51c97d050f
lukmaj02 <lmajcher02@gmail.com>
2023-12-26 20:07:28 +0100

    Added config-server and order-service, set prop for register-server and order-service in config-server

---
+++ config-server/src/test/java/com/App/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.App.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
