b902bd8bec24c8f81b9237e6ad5b731a63d9b68d
lukmaj02 <lmajcher02@gmail.com>
2024-02-08 18:19:46 +0100

    added oauth2-server, removed client-service, keycloak, changed config-server- fetching config from github

--- client-service/src/main/java/com/clientservice/Dto/RegistrationRequest.java
+++
@@ -1,4 +0,0 @@
-package com.clientservice.Dto;
-
-public record RegistrationRequest(String firstName, String lastName, String email, String password) {
-}
--- oauth2-server/src/test/java/com/app/oauth2server/Oauth2ServerApplicationTests.java
+++ oauth2-server/src/test/java/com/app/oauth2server/Oauth2ServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.clientservice.clientservice;
+package com.app.oauth2server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ClientServiceApplicationTests {
+class Oauth2ServerApplicationTests {
 
 	@Test
 	void contextLoads() {
